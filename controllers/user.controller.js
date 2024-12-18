8851af119493af677221d54e1a8fd93a2681a618
Pimpakarn26 <bonemarrow26bp@gmail.com>
2024-12-18 17:55:10 +0700

    new update to controllers

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,9 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const UserModel = require("../models/User");
 const salt = bcrypt.genSaltSync(10);
+require("dotenv").config();
+const secrect = "process";
 
 //priorlty จัดลดับความสำคัญ
 //register Libary:bcrypt  Step :1.check Username,Password 
@@ -34,3 +37,51 @@ exports.register = async (req, res) => {
     });
   }
 };
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    res.status(400).send({
+      message: "Please provide all requires fields!",
+    });
+    return;
+  }
+  try{
+    const userDoc = await UserModel.findOne({username});
+    if(!userDoc){
+      res.status(404).send({
+        message: "User not found",
+      });
+      return;
+    }
+    const isPasswordMatched = await bcrypt.compare(password, userDoc.password);
+    if(!isPasswordMatched){
+      res.status(401).send({
+        message: "Invalid credentials",
+      });
+      return;
+    }
+    //login success
+    jwt.sign({username, id:userDoc._id}, secrect,{}, (err, token)=>{
+      if(err){
+        res.status(500).send({
+          message:"Internal server error: Authentication Failed!"
+        });
+        return;
+      };   
+    //token generated
+    res.send({
+      message:"User logged in successfully",
+      id: userDoc._id,
+      username,
+      accessToken: token,
+    });
+  });
+ }catch (error){
+    res.status(500).send({
+      message:
+        error.message ||
+        "Something error occurred while registering a new user",
+    });
+  }
+}
--- routes/user.router.js
+++ routes/user.router.js
@@ -2,7 +2,12 @@ const express = require("express");
 const router = express.Router();
 const userControllers = require("../controllers/user.controller");
 
-//(Path) -- http://localhost:5000/api/v1/auth/register 
+//http://localhost:5000/api/v1/auth/register 
 router.post("/register", userControllers.register);
+
+//http://localhost:5000/api/v1/auth/login
+router.post("/login", userControllers.login);
+
+
 
 module.exports = router;
